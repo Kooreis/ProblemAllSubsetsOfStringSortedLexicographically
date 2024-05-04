84b6cf087c6e8dd24c44a94e65ddd7c9b99836d0
Kooreis <kooreis.official@gmail.com>
2024-05-03 23:29:38 -0500

    The first update includes the creation of the function getSubsets that takes a string as an argument and converts it into an array.

--- solution.cs
+++ solution.cs
@@ -1,14 +1,3 @@
-```javascript
 function getSubsets(string) {
     let array = Array.from(string);
-    let base = [''];
-    let results = array.reduce((previous, element) => {
-        return previous.concat(previous.map(value => {
-            return value + element;
-        }));
-    }, base);
-    return results.sort();
 }
-
-console.log(getSubsets('abc'));
-```
